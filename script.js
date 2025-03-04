625c6e52cd341233ca648f77cb09bb6a6185e957
EnricoCiccolini <piroroma90@gmail.com>
2025-03-04 14:02:47 +0100

    feat : add dom manipolation image whit button

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("ciao");
+const image = document.getElementById('lamp')
+console.log(image)
+const mainbutton = document.getElementById('button1')
+console.log(mainbutton)
+mainbutton.addEventListener('click', function() {
+    image.src =" img/yellow_lamp.png "
+    image.alt = "lampadina accesa"
+    console.log('hai cliccato')
+    
+})
